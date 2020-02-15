109f2df451b810b554f57ad5661f58c50b4c646b
thatguyvovich <vovichv2@ukr.net>
2020-02-15 19:12:39 +0000

    custom changes

--- calculator.cpp
+++ calculator.cpp
@@ -2,7 +2,7 @@
 
 int Calculator::Add (double a, double b)
 {
-	return a + b + 0.5;
+	return a + b;
 }
 
 int Calculator::Sub (double a, double b)
@@ -12,5 +12,13 @@ int Calculator::Sub (double a, double b)
 
 int Calculator::Mul (double a, double b)
 {
-    return a * b + 0.5;
+    return a * b;
 }
+
+// MY CREATION
+
+double Calculator::Div(double a, double b) {
+	return a / b;
+}
+
+// END MY CREATION
